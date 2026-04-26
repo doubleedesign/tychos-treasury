4db92caa3869108cfb481dd1b97dca2c764a167b
Leesa Ward <leesa@doubleedesign.com.au>
2026-04-27 09:14:24 +1000

    Fix: Generator template file paths; Feat: Have both API options ready in Vue component template

--- templates/TemplateName.vue
+++ templates/TemplateName.vue
@@ -1,10 +1,34 @@
+<!-- Choose an API style and remove the other one and the comments. -->
+<!-- Composition API -->
 <script setup lang="ts">
 export type TemplateNameProps = {
-
 };
 
 const props = defineProps<TemplateNameProps>();
-</script>;
+</script>
+
+<!-- Options API -->
+<script lang="ts">
+import { defineComponent } from 'vue';
+import type { PropType } from 'vue';
+
+export default defineComponent({
+	props: {
+	},
+	data() {
+		return {
+		};
+	},
+	computed: {
+	},
+	mounted() {
+	},
+	unmounted() {
+	},
+	methods: {
+	},
+});
+</script>
 
 <template>
 	<div class="template-name">
